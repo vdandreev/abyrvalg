13feabfad33b8e5593f6e99680654b2029c95ab5
Victor Andreev <v.d.andreev@gmail.com>
2017-12-31 15:22:21 +0300

    Tile nagic

--- field.py
+++ field.py
@@ -1,6 +1,32 @@
 from __future__ import print_function
 
+class Tile():
+    def __init__(self):
+        self.coord_x = 0
+        self.coord_y = 0
+        self.tile_contents = "water"
+        self.is_hit = False
 
+    def __repr__(self):
+        if self.tile_contents == "ship":
+            if self.is_hit:
+                return "[*]"
+            else:
+                return "[ ]"
+        if self.tile_contents == "water":
+            if self.is_hit:
+                return " * "   
+            else:
+                return " . "
+
+    def do_hit(self):
+        self.is_hit = True
+        return self.tile_contents
+        
+    def set_type(self, tile_type):
+        self.tile_contents = tile_type
+        
+    
 
 class Field():
     def __init__(self, rows = 10, cols = 10):
@@ -9,7 +35,7 @@ class Field():
         while len(local_grid) < rows:
             local_col = []
             while len(local_col) < cols:
-                local_col.append(".")
+                local_col.append(Tile())
             
             local_grid.append(local_col)
 
@@ -18,7 +44,7 @@ class Field():
     def draw(self):
         for i in self.grid:
             for j in i:
-                print (j+" ",end = '')
+                print (j,end = '')
             print ('')
             
             
@@ -28,6 +54,12 @@ class Field():
 
 field = Field()
 
-field.grid[2][4] = "!"
+field.grid[2][4].set_type("ship")
+field.grid[2][5].set_type("ship")
+field.grid[2][6].set_type("ship")
+
+
+field.grid[2][5].do_hit()
+field.grid[3][5].do_hit()
 
 field.draw()
